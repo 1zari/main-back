cf956d7d25d64866977feeec9083d8ed3c2847e2
rodzlen <qkqhrkxdkdy@naver.com>
2025-05-02 18:59:29 +0900

    :sparkles: add return job categories api

--- search/views/tree_views.py
+++ search/views/tree_views.py
@@ -6,23 +6,36 @@ from django.http import JsonResponse
 from django.views import View
 
 from config.settings.base import REDIS_DB, REDIS_HOST, REDIS_PORT
-from search.schemas import RegionTreeResponse
+from search.schemas import JonTreeResponse, RegionTreeResponse
+
+r = redis.Redis(
+    host=cast(str, REDIS_HOST),
+    port=cast(int, REDIS_PORT),
+    db=cast(int, REDIS_DB),
+    decode_responses=True,
+)
 
 
 class RegionTreeView(View):
     def get(self, request) -> JsonResponse:
-        r = redis.Redis(
-            host=cast(str, REDIS_HOST),
-            port=cast(int, REDIS_PORT),
-            db=cast(int, REDIS_DB),
-            decode_responses=True,
-        )
-
         region_tree_json = r.get("region_tree")
 
         region_tree = json.loads(region_tree_json) if region_tree_json else {}
 
         response_model = RegionTreeResponse.model_validate(region_tree)
+        return JsonResponse(
+            response_model.model_dump(),
+            status=200,
+            json_dumps_params={"ensure_ascii": False},
+        )
+
+
+class JobTreeView(View):
+    def get(self, request) -> JsonResponse:
+        job_tree_json = r.get("job_categories")
+        job_tree = json.loads(job_tree_json) if job_tree_json else {}
+
+        response_model = JonTreeResponse.model_validate(job_tree)
         return JsonResponse(
             response_model.model_dump(),
             status=200,
