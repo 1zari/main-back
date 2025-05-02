9bbac772701e2a025959c603c37857d26a25a1f6
rodzlen <qkqhrkxdkdy@naver.com>
2025-05-02 19:01:33 +0900

    :sparkles: create and fix search app test code

--- search/tests/test_tree.py
+++ search/tests/test_tree.py
@@ -4,7 +4,7 @@ import pytest
 import redis
 from django.test import Client
 
-from search.redis_script import REDIS_CONFIG
+from search.redis_script import r as re
 
 
 @pytest.mark.django_db
@@ -12,7 +12,7 @@ def test_region_tree_view_with_real_redis():
     """
     mock 데이터 없이 실제 redis에
     """
-    r = redis.Redis(**REDIS_CONFIG)
+    r = re
 
     client = Client()
     url = "/api/search/region/"
@@ -24,3 +24,23 @@ def test_region_tree_view_with_real_redis():
     data = json.loads(response.content)
 
     assert data == json.loads(r.get("region_tree"))
+
+
+@pytest.mark.django_db
+def test_job_tree_view_with_real_redis():
+    """
+    mock 데이터 없이 실제 redis에
+    """
+    r = re
+
+    client = Client()
+    url = "/api/search/job/"
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert response["Content-Type"] == "application/json"
+
+    data = json.loads(response.content)
+    print(data)
+
+    assert data == json.loads(r.get("job_categories"))
