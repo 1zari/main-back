53e6d45be26fc13d03ecc65ac8a9f84b51d2154e
Anianim <133176415+Anianim@users.noreply.github.com>
2025-05-04 18:26:02 +0900

    :sparkles: Add csrf view

--- config/urls/dev_urls.py
+++ config/urls/dev_urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from utils.csrf_views import get_csrf_token
 urlpatterns = [
     path("api/admin/", admin.site.urls),
     path("api/resume/", include("resume.urls.resume_urls")),
@@ -8,4 +8,5 @@ urlpatterns = [
     path("api/job-postings/", include("job_posting.urls.urls")),
     path("api/user/", include("user.urls.urls")),
     path("api/search/", include("search.urls.search_urls")),
+    path("api/csrf/", get_csrf_token),
 ]
