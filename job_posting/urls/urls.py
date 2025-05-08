8b9aafa3519689496458854617e9c6f6c560b406
Anianim <133176415+Anianim@users.noreply.github.com>
2025-05-08 15:24:56 +0900

    :bug: fix posting urls

--- job_posting/urls/urls.py
+++ job_posting/urls/urls.py
@@ -9,16 +9,20 @@ from ..views.views import (
 urlpatterns = [
     # 공고 리스트 조회
     path("", JobPostingListView.as_view(), name="job_posting_list"),
-
     # 공고 상세 조회, 수정, 삭제
-    path("<uuid:job_posting_id>/", JobPostingDetailView.as_view(), name="job_posting_detail"),
-
+    path(
+        "<uuid:job_posting_id>/",
+        JobPostingDetailView.as_view(),
+        name="job_posting_detail",
+    ),
     # 공고 생성
     path("create/", JobPostingDetailView.as_view(), name="job_posting_create"),
-
     # 북마크 목록 조회
     path("bookmark/", JobPostingBookmarkView.as_view(), name="bookmark_list"),
-
     # 북마크 등록/삭제
-    path("bookmark/<uuid:job_posting_id>/", JobPostingBookmarkView.as_view(), name="bookmark_toggle"),
+    path(
+        "bookmark/<uuid:job_posting_id>/",
+        JobPostingBookmarkView.as_view(),
+        name="bookmark_toggle",
+    ),
 ]
