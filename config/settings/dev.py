7adfc176ccf94f172209985f895a9654e9c738b8
Anianim <133176415+Anianim@users.noreply.github.com>
2025-05-12 16:23:47 +0900

    :bug: fix jobposting

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -5,11 +5,11 @@ from .base import *
 ROOT_URLCONF = "config.urls.dev_urls"
 
 # 로컬과 CI 환경 구분
-
-# CI 환경에서 사용하는 경로 (Ubuntu)
-GDAL_LIBRARY_PATH = "/usr/lib/x86_64-linux-gnu/libgdal.so"
-GEOS_LIBRARY_PATH = "/usr/lib/x86_64-linux-gnu/libgeos_c.so"
-
-# 로컬 환경에서 사용하는 경로 (macOS)
-# GDAL_LIBRARY_PATH = "/opt/homebrew/lib/libgdal.dylib"
-# GEOS_LIBRARY_PATH = "/opt/homebrew/lib/libgeos_c.dylib"
+if os.environ.get("CI") == "true":
+    # CI 환경에서 사용하는 경로 (Ubuntu)
+    GDAL_LIBRARY_PATH = "/usr/lib/x86_64-linux-gnu/libgdal.so"
+    GEOS_LIBRARY_PATH = "/usr/lib/x86_64-linux-gnu/libgeos_c.so"
+else:
+    # 로컬 환경에서 사용하는 경로 (macOS)
+    GDAL_LIBRARY_PATH = "/opt/homebrew/lib/libgdal.dylib"
+    GEOS_LIBRARY_PATH = "/opt/homebrew/lib/libgeos_c.dylib"
