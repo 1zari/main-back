e5b151ce185cb0daa19bd401b8b3a4cd5fcaa299
rodzlen <qkqhrkxdkdy@naver.com>
2025-05-16 01:04:19 +0900

    🐛 add GDAL, GEOS path when linux

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -9,6 +9,11 @@ if os.environ.get("CI") == "true":
     # CI 환경에서 사용하는 경로 (Ubuntu)
     GDAL_LIBRARY_PATH = "/usr/lib/x86_64-linux-gnu/libgdal.so"
     GEOS_LIBRARY_PATH = "/usr/lib/x86_64-linux-gnu/libgeos_c.so"
+
+elif os.environ.get("USER") == "root":
+    # wsl Ubuntu 실행시
+    GDAL_LIBRARY_PATH = "/usr/lib/x86_64-linux-gnu/libgdal.so"
+    GEOS_LIBRARY_PATH = "/usr/lib/x86_64-linux-gnu/libgeos_c.so"
 else:
     # 로컬 환경에서 사용하는 경로 (macOS)
     GDAL_LIBRARY_PATH = "/opt/homebrew/lib/libgdal.dylib"
